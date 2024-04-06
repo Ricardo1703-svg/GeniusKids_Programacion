f124d5ebbec03a0620edd180fb3d248c7176921a
Ricardo1703-svg <ralvares961@gmail.com>
2024-04-05 19:07:49 -0600

    first commit

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,8 @@
+<!------------- Barra de Navegacion ---------------------->
+<nav>
+    <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/nosotros">Acerca de Nosotros</a></li>
+    </ul>
+</nav>
+<!-- ------------------------------------------------- -->
---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "/css/app.css">
+    <title>GeniusKids</title>
+</head>
+<body>
+    <header>
+        @include('includes.navbar')
+    </header>
+<h1>Hola Master</h1>
+</body>
+<footer>
+        <p>&copy; 2024 GeniusKids. Todos los derechos reservados.</p>
+    </footer>   
+</html>
